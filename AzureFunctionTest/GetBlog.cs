a9d93da8d1539b550f6a2034bad0574c15716ad5
xufeng <zzhxufeng@163.com>
2022-11-20 11:17:30 +0800

    First post test

---
+++ AzureFunctionTest/GetBlog.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using SharedModels;
+
+namespace AzureFunctionTest
+{
+    public static class GetBlog
+    {
+        [FunctionName("GetBlog")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+
+            Blog blog = new Blog();
+            blog.Title = "第一篇Post";
+            blog.Body = "这是内容";
+            blog.Created = DateTime.Now;
+
+            string jsonRes = JsonConvert.SerializeObject(blog);
+
+            return new OkObjectResult(jsonRes);
+        }
+    }
+}
---
+++ SharedModels/Blog.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedModels
+{
+    public class Blog
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
